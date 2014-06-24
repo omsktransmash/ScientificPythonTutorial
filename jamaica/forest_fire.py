41d5170fbd0184babe257511d35072e67c98257b
ar8844 <ar8844@business.kaist.ac.kr>
2014-06-24 23:38:44 +0900

    Revert "asdf"
    This reverts commit 01cd4d6c9eae80c892a3ce3b75d3ed70f8cec1ab.

--- jamaica/forest_fire.py
+++ jamaica/forest_fire.py
@@ -6,24 +6,18 @@ class Tree:
     def grow(self):
         self.grown = True
 
-    def burn(self,my_row_index,my_col_index,trees):
+    def burn(self,my_index,trees):
 
         if self.grown == True:
             
             self.grown = False
 
-            if my_col_index > 0:
-                left_tree = trees[my_row_index][my_col_index - 1]
-                left_tree.burn(my_row_index, my_col_index - 1,trees)
-            if my_col_index < len(trees) -1:
-                right_tree = trees[my_row_index][my_col_index + 1]
-                right_tree.burn(my_row_index, my_col_index +1, trees)
-            if my_row_index > 0:
-                up_tree = trees[my_row_index -1][my_col_index]
-                up_tree.burn(my_row_index -1,my_col_index,trees)
-            if my_row_index < len(trees) -1:
-                down_tree = trees[my_row_index + 1][my_col_index]
-                down_tree.burn(my_row_index +1,my_col_index, trees)
+            if my_index > 0:
+                left_tree = trees[my_index - 1]
+                left_tree.burn(my_index - 1,trees)
+            if my_index < len(trees) -1:
+                right_tree = trees[my_index + 1]
+                right_tree.burn(my_index +1, trees)
     
     def __init__(self):
         self.p = 0.2    
@@ -32,57 +26,40 @@ class Tree:
 f = 0.05
 
 max_step = 10
-tree_row = 5
-tree_col = 5
+num_trees = 30
 
-trees_row = []
-trees_col = []
 trees = []
-##making land for trees
-for my_row_index in range(tree_row):
-    for my_col_index in range(tree_col):
-        trees_row.append(Tree())
-        trees_col.append(Tree())
-        trees.append(Tree)
-        
+for index in range(num_trees):
+    trees.append(Tree())
 
-##growing trees
+
 for step in range(max_step):
     
-    for row_tree in trees_row:
-        for col_tree in trees_col:
-            if random.random() <= row_tree.p:
-                row_tree.grow()
-            if random.random() <= col_tree.p:
-                col_tree.grow()
+    for tree in trees:
+        if random.random() <= tree.p:
+            tree.grow()
 
-
-##trees burning  
-    for t_row_index in range(0,len(trees_row)):
-        for t_col_index in range(0,len(trees_col)):
-            row_tree=trees[t_row_index]
-            col_tree=trees[t_col_index]
-            if random.random() <= f:
-                row_tree.burn(t_row_index,trees)
-                col_tree.burn(t_col_index,trees)
+    
+    for t_index in range(0,len(trees)):
+        tree = trees[t_index]
+        if random.random() <= f:
+            tree.burn(t_index,trees)
     
     count = 0
-    for row_tree in range(tree_row):
-        for col_tree in range(tree_col):
-            if tree[row_tree][col_tree].grown:
-                count = count +1
-     
+    for tree in trees:
+        if tree.grown:
+            count = count + 1
 
     #print count
-    for row_tree in range(tree_row):
-        for col_tree in range(tree_col):
-            if tree[row_tree][col_tree].grown:
-                print "1"
-            else:
-                print "0"
 
-        print ""
-  
+    for tree in trees:
+        if tree.grown:
+            print "1",
+        else:
+            print "0",
+            
+    print ""
+
 
 
 
